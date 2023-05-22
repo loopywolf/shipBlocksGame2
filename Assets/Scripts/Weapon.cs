bceba3b8bcfb615c91d6d300f05e8189a37f90cd
Andrew Pidcock <loopywolf@gmail.com>
2023-05-22 11:40:44 -0400

    fixed bug: fire when clicking OK

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject BulletPrefab;
+    public EventSystem eventSystem;
 
     // Update is called once per frame
     void Update()
     {
+        if (eventSystem.IsPointerOverGameObject())
+        {
+
+        } else
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
