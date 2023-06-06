9f7804b51781e3394ba72f1f56be38feeb960a45
Andrew Pidcock <loopywolf@gmail.com>
2023-06-06 11:07:27 -0400

    made camera independent

---
+++ Assets/SmoothFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public float damping;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        Vector3 movePosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, movePosition, ref velocity, damping);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}//class
